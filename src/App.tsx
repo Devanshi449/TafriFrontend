9472448977ea733f8d976525f8d2e42b59ad908c
diya garg <84235311+Diyagarg-15@users.noreply.github.com>
2024-09-12 16:26:48 +0530

    chage

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div>
-      <p className='bg-blue-200'>Hellooooooo</p>
+      <p className='bg-blue-200'>Diya</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
