861edfffa0828ffa6bc98bbd3005e9f7a3723903
Devanshi Gupta <devanshigupta2002@gmail.com>
2024-09-25 10:25:57 +0530

    added/proxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,54 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+interface User {
+  packageId: number;
+  supplierId: number;
+  duration: number;
+  packageName: string;
+  description: string;
+  flightBooking: boolean;
+  accommodation: boolean;
+  sightseeing: boolean;
+  luxuryFacilities: string;
+  availableCount: number;
+  supplierPrice: number;
+  isPublic: boolean;
+  createdAt: string;
+  finalPrice: number;
+  totalSeats: number;
+  packageImage: string;
+  destination: string;
+}
+
 function App() {
   const [count, setCount] = useState(0)
+  // const [userData, setUserData] = useState<User[]>([]); 
+
+  // useEffect(() => {
+  //   // Call the API when the component is mounted
+  //   fetch("/api/User/GetData", {
+  //     method: "GET",
+  // headers: {
+  //   "Accept": "text/plain",
+  // },
+  //   })
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       // Update the state with the fetched data
+  //       setUserData(data)
+  //     })
+  //     .catch((error) => {
+  //       console.error("Error fetching data:", error)
+  //     })
+  // }, []) // Empty dependency array ensures it runs only once when component is mounted
 
   return (
     <>
       <div>
-      <p className='bg-blue-200'>Diya</p>
+        <p className='bg-blue-200'>Diya</p>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -29,6 +68,19 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+
+      {/* Display the fetched user data */}
+      {/* <div>
+        <h2>User Data</h2>
+        <ul>
+          {userData.map((user, index) => (
+            <li key={index}>
+            <p><strong>Package Name:</strong> {user?.packageName}</p>
+           
+          </li>
+          ))}
+        </ul>
+      </div> */}
     </>
   )
 }
